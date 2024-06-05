f6c663674cd760e5a6c11b9e74e55f9eb4599291
hshsthecoder <jack.catchpole@icloud.com>
2024-06-05 11:46:00 +0930

    Add files via upload

--- pytyper.py
+++ pytyper.py
@@ -1,16 +1,24 @@
+#import libs
 import os
 from time import sleep
-import write
-import reader
+
+#program start
 print("Welcome To Pytyper by Jack")
-r_or_w = input("enter W to write or any other key to read\n")
-if r_or_w == 'w':
-    write.typing()
+doc_name = input("Name Output File (txt format)")
+line_num = int(input("enter aprox line numbers needed (cant add more later so good luck)"))
+file = open(doc_name + ".txt", 'w')
+while line_num > 0:
+    lines_left = str(line_num)
+    print(lines_left + "lines remaining")
+    file = open(doc_name + ".txt", 'a')
+    text = input ("start typing below\n")
+    file.write(text+"\n")
+    line_num = line_num - 1
 
 
-##To-Do
-##Saving in other folders
-##line break support
-##add reading mode
-##make reading mode less "hacky"
-##make own file ext that pytyper can open
+#To-Do
+#Saving in other folders
+#line break support
+#add reading mode
+#make reading mode less "hacky"
+#make own file ext that pytyper can open
