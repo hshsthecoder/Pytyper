429abe73ac08c23054753250f1857eba69abeeff
hshsthecoder <jack.catchpole@icloud.com>
2024-06-11 18:01:45 +0930

    added pytype file ext

--- pytyper.py
+++ pytyper.py
@@ -10,16 +10,16 @@ print("IMPORTANT: READING AND OR EDITING FILES NOT YET SUPORTED")
 open_read = int(input("are we wanting to read a file (1 is yes 0 is no)"))
 if open_read == 1:
     file_read_name = input("Enter file path: ")    
-    print(open(file_read_name, 'r').read()) 
+    print(open(file_read_name + ".pytype", 'r').read()) 
     
 else:
-    doc_name = input("Name Output File (txt format)")
+    doc_name = input("Name Output File (txt format with .pytype extention)")
     line_num = int(input("enter aprox line numbers needed (cant add more later so good luck)"))
     file = open(doc_name + ".txt", 'w')
     while line_num > 0:
         lines_left = str(line_num)
         print(lines_left + "lines remaining")
-        file = open(doc_name + ".txt", 'a')
+        file = open(doc_name + ".pytype", 'a')
         text = input ("start typing below\n")
         file.write(text+"\n")
         line_num = line_num - 1
